84981a22fb8d253f98053dc33eb1ca988102be70
algnot <thanawat.tongla@gmail.com>
2026-09-10 17:32:23 +0700

    feat: fix uo

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://cms.fastforward-sport.com'),
+  proxy: env.bool('IS_PROXIED', true),
   app: {
     keys: env.array('APP_KEYS'),
   },
