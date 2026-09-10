b171a7f1b69380713d8965a01d76de6996771e0b
algnot <thanawat.tongla@gmail.com>
2026-09-10 17:44:50 +0700

    fix: update PUBLIC_URL to correct domain for server configuration

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://cms.fastforward-sport.com'),
+  url: env('PUBLIC_URL', 'https://cms.fastforwardsport.com'),
   proxy: env.bool('IS_PROXIED', true),
   app: {
     keys: env.array('APP_KEYS'),
